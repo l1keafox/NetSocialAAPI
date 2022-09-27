4cb956bd9d095fcfb3173a37c87e085d32e74ea7
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-09-27 15:10:08 -0600

    updated reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,6 +1,28 @@
 const { Schema, Types } = require('mongoose');
 
+
+// (SCHEMA ONLY)
 const reactSchema = new Schema(
+    {
+        reactionId:{
+            // Use Mongoose's ObjectId data type
+            // Default value is set to a new ObjectId
+        },
+        reactionBody:{
+            type:String,
+            required:true,
+            maxLength: 280,
+        },
+        userName:{
+            type:String,
+            required:true,
+        },
+        createdAt:{
+            type:Date,
+            // Set default value to the current timestamp
+            // Use a getter method to format the timestamp on query            
+        }
+    },
     {
         toJSON: {
           getters: true,
@@ -10,4 +32,8 @@ const reactSchema = new Schema(
     
     );
     
+    // Schema Settings
+
+    // This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+
 module.exports = reactSchema;
