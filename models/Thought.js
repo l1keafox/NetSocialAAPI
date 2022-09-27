a7458c2e50cadadbe2ac7e2d5a000e0448ca8502
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-09-27 15:08:27 -0600

    updated thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,14 +1,37 @@
-const { Schema, Types } = require('mongoose');
-
+const { Schema, Types } = require("mongoose");
 
 const thoughtSchema = new Schema(
-    {
-        toJSON: {
-          getters: true,
-        },
-        id: false,
-        }
-    
-    );
-    
-    module.exports = thoughtSchema;
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      // Set default value to the current timestamp
+      // Use a getter method to format the timestamp on query
+    },
+    userName: {
+      //creator
+      type: String,
+      required: true,
+    },
+    reactions: {
+      // sort of like replies.
+      //Array of nested documents created with the reactionSchema
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+//     Schema Settings
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+module.exports = thoughtSchema;
