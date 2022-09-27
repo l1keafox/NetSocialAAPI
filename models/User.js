44c7a902012ab70e4f079890c58642eff9514e1f
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-09-27 15:06:09 -0600

    updated user schema

--- models/User.js
+++ models/User.js
@@ -2,6 +2,28 @@ const { Schema, Types } = require('mongoose');
 
 const userSchema = new Schema(
     {
+        username:{
+            //String,unique,required,trimmed
+            type:String,
+            required: true,
+            unique:true,
+            trim: true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            // Must match a valid email address
+            validate: [ isEmail, 'invalid email' ]
+        },
+        thoughts:{
+//Array of _id values referencing the Thought model
+        },
+        friends:{
+//Array of _id values referencing the User model (self-reference)
+        },
+    },
+    {
     toJSON: {
       getters: true,
     },
@@ -9,5 +31,8 @@ const userSchema = new Schema(
     }
 
 );
+
+
+//Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 
 module.exports = userSchema;
